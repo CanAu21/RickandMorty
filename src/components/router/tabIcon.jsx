b3a4dbdee658d778f9220ffe921f5d64ce1bb579
CanAu21 <furkan.adrian9@outlook.com>
2024-05-05 22:24:02 -0700

    Changed first tabIcon

--- src/components/router/tabIcon.jsx
+++ src/components/router/tabIcon.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import {CHARACTERS, EPISODES, LOCATIONS, SETTINGS} from '../../utils/routes';
-import {Home, Location, PresentionChart, Setting2} from 'iconsax-react-native';
+import {
+  Android,
+  Home,
+  Location,
+  PresentionChart,
+  Setting2,
+} from 'iconsax-react-native';
 
 const TabIcon = ({screenName, color, focused, size}) => {
   if (screenName == CHARACTERS)
     return (
-      <Home variant={focused ? 'Bold' : 'Outline'} size={size} color={color} />
+      <Android
+        variant={focused ? 'Bold' : 'Outline'}
+        size={size}
+        color={color}
+      />
     );
   if (screenName == EPISODES)
     return (
