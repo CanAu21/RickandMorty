1246e6ee726aa2501b4db95a506e1a37f9f78225
CanAu21 <furkan.adrian9@outlook.com>
2024-05-08 21:04:18 -0700

    Created store and called characters name

--- src/screens/characters/index.jsx
+++ src/screens/characters/index.jsx
@@ -1,11 +1,23 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {View, FlatList, Text} from 'react-native';
+import React, {useEffect} from 'react';
 import {screensStyle} from '../../styles/screensStyle';
+import {useDispatch, useSelector} from 'react-redux';
+import {getCharacterList} from '../../store/actions/charactersActions';
 
 const Characters = () => {
+  const dispatch = useDispatch();
+  const {characterList} = useSelector(state => state.characters);
+
+  useEffect(() => {
+    dispatch(getCharacterList());
+  }, []);
+
   return (
     <View style={screensStyle.container}>
-      <Text>Characters</Text>
+      <FlatList
+        data={characterList}
+        renderItem={({item}) => <Text>{item.name}</Text>}
+      />
     </View>
   );
 };
